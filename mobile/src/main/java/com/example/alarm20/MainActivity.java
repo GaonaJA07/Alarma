d97945856a2afbf455196bd8cdd48f070418292d
Jose Antonio Garcia Gaona <301910214@ute.edu.mx>
2022-06-29 20:14:51 -0500

    Inicial

--- mobile/src/main/java/com/example/alarm20/MainActivity.java
+++ mobile/src/main/java/com/example/alarm20/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.alarm20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
@@ -19,16 +20,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     //Declaracion de la variable
+        //Se cambio txtEti > txtEtii.
         txtEtii = (TextView) findViewById(R.id.etiSeleccion);
         switchE = (Switch) findViewById(R.id.idSwitch);
     }
     public void onclick(View view){
         if (view.getId()==R.id.idSwitch){
+
+            //cejemplovmdkvmdd
+
+
             //Es checar si esta activo la Opcion.
             if (switchE.isChecked()){
                 txtEtii.setText("Fue activado");
+                Intent intent = new Intent(this, SegundoActivity.class);
+                startActivity(intent);
+                ////Aqui va ir el proceso si se llegara activar el bluetooth
             }else{
                 txtEtii.setText("Fue desactivado");
+                //Aqui va ir el proceso si se llegara activar el bluetooth
             }
         }
     }
